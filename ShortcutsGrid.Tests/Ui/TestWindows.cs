3a984d139d65f0a886a720852380ce18dd5fa47c
Minko <minkostaev@yahoo.com>
2022-03-04 00:27:00 +0200

    test: about window

--- ShortcutsGrid.Tests/Ui/TestWindows.cs
+++ ShortcutsGrid.Tests/Ui/TestWindows.cs
@@ -1,45 +1,55 @@
-﻿namespace ShortcutsGrid.Tests.Ui
+﻿namespace ShortcutsGrid.Tests.Ui;
+
+using NUnit.Framework;
+using ShortcutsGrid.Windows;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+[TestFixture]
+internal class TestWindows
 {
-    using NUnit.Framework;
-    using System.IO;
-    using System.Reflection;
-    using System.Threading;
-
-    [TestFixture]
-    internal class TestWindows
+    
+    [Test]
+    [Apartment(ApartmentState.STA)]
+    public void TestMainWindow()
     {
-        
-        [Test]
-        [Apartment(ApartmentState.STA)]
-        public void TestMainWindow()
-        {
-            string testhost = "testhost.csv";
-            string csvContent = GetInputFile(testhost);
-            File.WriteAllText(testhost, csvContent);
+        string testhost = "testhost.csv";
+        string csvContent = GetInputFile(testhost);
+        File.WriteAllText(testhost, csvContent);
 
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
 
-            if (File.Exists(testhost))
-                File.Delete(testhost);
+        if (File.Exists(testhost))
+            File.Delete(testhost);
 
-            Assert.IsInstanceOf<MainWindow>(mainWindow);
-        }
-
-        public string GetInputFile(string fileName)
-        {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-
-            string content = string.Empty;
-            var stream = assembly.GetManifestResourceStream("ShortcutsGrid.Tests.Resources." + fileName);
-            if (stream != null)
-            {
-                var reader = new StreamReader(stream);
-                content = reader.ReadToEnd();
-            }
-
-            return content;
-        }
-
+        Assert.IsInstanceOf<MainWindow>(mainWindow);
     }
+
+    [Test]
+    [Apartment(ApartmentState.STA)]
+    public void TestAbout()
+    {
+        About about = new About();
+        ///about.Show();
+
+        Assert.IsInstanceOf<About>(about);
+    }
+
+    public string GetInputFile(string fileName)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        string content = string.Empty;
+        var stream = assembly.GetManifestResourceStream("ShortcutsGrid.Tests.Resources." + fileName);
+        if (stream != null)
+        {
+            var reader = new StreamReader(stream);
+            content = reader.ReadToEnd();
+        }
+
+        return content;
+    }
+
 }
--- ShortcutsGrid/Services/Image/ExtractIcon.cs
+++ ShortcutsGrid/Services/Image/ExtractIcon.cs
@@ -1,117 +1,116 @@
-﻿namespace ShortcutsGrid.Services.Image
+﻿namespace ShortcutsGrid.Services.Image;
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Drawing;
+
+internal static class ExtractIcon
 {
-    using System;
-    using System.IO;
-    using System.Collections.Generic;
-    using System.Runtime.InteropServices;
-    using System.Security;
-    using System.Drawing;
+    [UnmanagedFunctionPointer(CallingConvention.Winapi, SetLastError = true, CharSet = CharSet.Unicode)]
+    [SuppressUnmanagedCodeSecurity]
+    internal delegate bool ENUMRESNAMEPROC(IntPtr hModule, IntPtr lpszType, IntPtr lpszName, IntPtr lParam);
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    public static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hFile, uint dwFlags);
 
-    internal static class ExtractIcon
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    public static extern IntPtr FindResource(IntPtr hModule, IntPtr lpName, IntPtr lpType);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr LoadResource(IntPtr hModule, IntPtr hResInfo);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr LockResource(IntPtr hResData);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern uint SizeofResource(IntPtr hModule, IntPtr hResInfo);
+
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    [SuppressUnmanagedCodeSecurity]
+    public static extern bool EnumResourceNames(IntPtr hModule, IntPtr lpszType, ENUMRESNAMEPROC lpEnumFunc, IntPtr lParam);
+
+
+    private const uint LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
+    private readonly static IntPtr RT_ICON = (IntPtr)3;
+    private readonly static IntPtr RT_GROUP_ICON = (IntPtr)14;
+
+    public static Icon ExtractIconFromExecutable(string path)
     {
-        [UnmanagedFunctionPointer(CallingConvention.Winapi, SetLastError = true, CharSet = CharSet.Unicode)]
-        [SuppressUnmanagedCodeSecurity]
-        internal delegate bool ENUMRESNAMEPROC(IntPtr hModule, IntPtr lpszType, IntPtr lpszName, IntPtr lParam);
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hFile, uint dwFlags);
+        IntPtr hModule = LoadLibraryEx(path, IntPtr.Zero, LOAD_LIBRARY_AS_DATAFILE);
+        var tmpData = new List<byte[]>();
 
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern IntPtr FindResource(IntPtr hModule, IntPtr lpName, IntPtr lpType);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern IntPtr LoadResource(IntPtr hModule, IntPtr hResInfo);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern IntPtr LockResource(IntPtr hResData);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern uint SizeofResource(IntPtr hModule, IntPtr hResInfo);
-
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        [SuppressUnmanagedCodeSecurity]
-        public static extern bool EnumResourceNames(IntPtr hModule, IntPtr lpszType, ENUMRESNAMEPROC lpEnumFunc, IntPtr lParam);
-
-
-        private const uint LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
-        private readonly static IntPtr RT_ICON = (IntPtr)3;
-        private readonly static IntPtr RT_GROUP_ICON = (IntPtr)14;
-
-        public static Icon ExtractIconFromExecutable(string path)
+        ENUMRESNAMEPROC callback = (h, t, name, l) =>
         {
-            IntPtr hModule = LoadLibraryEx(path, IntPtr.Zero, LOAD_LIBRARY_AS_DATAFILE);
-            var tmpData = new List<byte[]>();
+            var dir = GetDataFromResource(hModule, RT_GROUP_ICON, name);
 
-            ENUMRESNAMEPROC callback = (h, t, name, l) =>
+            // Calculate the size of an entire .icon file.
+
+            int count = BitConverter.ToUInt16(dir, 4);  // GRPICONDIR.idCount
+            int len = 6 + 16 * count;                   // sizeof(ICONDIR) + sizeof(ICONDIRENTRY) * count
+            for (int i = 0; i < count; ++i)
+                len += BitConverter.ToInt32(dir, 6 + 14 * i + 8);   // GRPICONDIRENTRY.dwBytesInRes
+
+            using (var dst = new BinaryWriter(new MemoryStream(len)))
             {
-                var dir = GetDataFromResource(hModule, RT_GROUP_ICON, name);
+                // Copy GRPICONDIR to ICONDIR.
 
-                // Calculate the size of an entire .icon file.
+                dst.Write(dir, 0, 6);
 
-                int count = BitConverter.ToUInt16(dir, 4);  // GRPICONDIR.idCount
-                int len = 6 + 16 * count;                   // sizeof(ICONDIR) + sizeof(ICONDIRENTRY) * count
+                int picOffset = 6 + 16 * count; // sizeof(ICONDIR) + sizeof(ICONDIRENTRY) * count
+
                 for (int i = 0; i < count; ++i)
-                    len += BitConverter.ToInt32(dir, 6 + 14 * i + 8);   // GRPICONDIRENTRY.dwBytesInRes
-
-                using (var dst = new BinaryWriter(new MemoryStream(len)))
                 {
-                    // Copy GRPICONDIR to ICONDIR.
+                    // Load the picture.
 
-                    dst.Write(dir, 0, 6);
+                    ushort id = BitConverter.ToUInt16(dir, 6 + 14 * i + 12);    // GRPICONDIRENTRY.nID
+                    var pic = GetDataFromResource(hModule, RT_ICON, (IntPtr)id);
 
-                    int picOffset = 6 + 16 * count; // sizeof(ICONDIR) + sizeof(ICONDIRENTRY) * count
+                    // Copy GRPICONDIRENTRY to ICONDIRENTRY.
 
-                    for (int i = 0; i < count; ++i)
-                    {
-                        // Load the picture.
+                    dst.Seek(6 + 16 * i, 0);
 
-                        ushort id = BitConverter.ToUInt16(dir, 6 + 14 * i + 12);    // GRPICONDIRENTRY.nID
-                        var pic = GetDataFromResource(hModule, RT_ICON, (IntPtr)id);
+                    dst.Write(dir, 6 + 14 * i, 8);  // First 8bytes are identical.
+                    dst.Write(pic.Length);          // ICONDIRENTRY.dwBytesInRes
+                    dst.Write(picOffset);           // ICONDIRENTRY.dwImageOffset
 
-                        // Copy GRPICONDIRENTRY to ICONDIRENTRY.
+                    // Copy a picture.
 
-                        dst.Seek(6 + 16 * i, 0);
+                    dst.Seek(picOffset, 0);
+                    dst.Write(pic, 0, pic.Length);
 
-                        dst.Write(dir, 6 + 14 * i, 8);  // First 8bytes are identical.
-                        dst.Write(pic.Length);          // ICONDIRENTRY.dwBytesInRes
-                        dst.Write(picOffset);           // ICONDIRENTRY.dwImageOffset
-
-                        // Copy a picture.
-
-                        dst.Seek(picOffset, 0);
-                        dst.Write(pic, 0, pic.Length);
-
-                        picOffset += pic.Length;
-                    }
-
-                    tmpData.Add(((MemoryStream)dst.BaseStream).ToArray());
+                    picOffset += pic.Length;
                 }
-                return true;
-            };
-            EnumResourceNames(hModule, RT_GROUP_ICON, callback, IntPtr.Zero);
-            byte[][] iconData = tmpData.ToArray();
-            using (var ms = new MemoryStream(iconData[0]))
-            {
-                return new Icon(ms);
+
+                tmpData.Add(((MemoryStream)dst.BaseStream).ToArray());
             }
-        }
-
-        private static byte[] GetDataFromResource(IntPtr hModule, IntPtr type, IntPtr name)
+            return true;
+        };
+        EnumResourceNames(hModule, RT_GROUP_ICON, callback, IntPtr.Zero);
+        byte[][] iconData = tmpData.ToArray();
+        using (var ms = new MemoryStream(iconData[0]))
         {
-            // Load the binary data from the specified resource.
-
-            IntPtr hResInfo = FindResource(hModule, name, type);
-
-            IntPtr hResData = LoadResource(hModule, hResInfo);
-
-            IntPtr pResData = LockResource(hResData);
-
-            uint size = SizeofResource(hModule, hResInfo);
-
-            byte[] buf = new byte[size];
-            Marshal.Copy(pResData, buf, 0, buf.Length);
-
-            return buf;
+            return new Icon(ms);
         }
-
     }
+
+    private static byte[] GetDataFromResource(IntPtr hModule, IntPtr type, IntPtr name)
+    {
+        // Load the binary data from the specified resource.
+
+        IntPtr hResInfo = FindResource(hModule, name, type);
+
+        IntPtr hResData = LoadResource(hModule, hResInfo);
+
+        IntPtr pResData = LockResource(hResData);
+
+        uint size = SizeofResource(hModule, hResInfo);
+
+        byte[] buf = new byte[size];
+        Marshal.Copy(pResData, buf, 0, buf.Length);
+
+        return buf;
+    }
+
 }
