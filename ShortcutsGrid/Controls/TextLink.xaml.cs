8255e9841d16441f618f6678d57df4cd54cd2d92
Minko <minkostaev@yahoo.com>
2022-06-23 00:25:14 +0300

    feat: new cntrol TextLink

---
+++ ShortcutsGrid/Controls/TextLink.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ShortcutsGrid.Controls;
+
+using ShortcutsGrid.Services.Run;
+using System.Windows.Controls;
+
+/// <summary>
+/// Interaction logic for TextLink.xaml
+/// </summary>
+public partial class TextLink : UserControl
+{
+    public TextLink()
+    {
+        InitializeComponent();
+        hprLnk.NavigateUri = new System.Uri("");
+        tbText.Text = "";
+        hprLnk.Click += delegate { RunProcess.OpenLink(hprLnk.NavigateUri.ToString()); };
+    }
+}
