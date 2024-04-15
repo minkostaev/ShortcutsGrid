638246689f6d6b61ec78ab93686be0a5d5aa2e54
Minko Staev <minkostaev@yahoo.com>
2024-04-16 01:34:01 +0300

    chore: fix pipeline

--- ShortcutsGrid.Tests/Services/TestErrorDialog.cs
+++ ShortcutsGrid.Tests/Services/TestErrorDialog.cs
@@ -9,20 +9,20 @@ using System.Windows;
 [TestFixture]
 internal class TestErrorDialog
 {
-    //[Test]
-    //public void TestErrorDialog_OK()
-    //{
-    //    var messageBoxMock = new Mock<IMessageBoxWrapper>();
-    //    messageBoxMock.Setup(m => m.Show(It.IsAny<string>(), It.IsAny<string>(), MessageBoxButton.OK, MessageBoxImage.Error))
-    //        .Returns(MessageBoxResult.OK); //can be whatever you need, depends on test case
-    //    var sut = new MessageDialogs(messageBoxMock.Object);
+    [Test]
+    public void TestErrorDialog_OK()
+    {
+        var messageBoxMock = new Mock<IMessageBoxWrapper>();
+        messageBoxMock.Setup(m => m.Show(It.IsAny<string>(), It.IsAny<string>(), MessageBoxButton.OK, MessageBoxImage.Error))
+            .Returns(MessageBoxResult.OK); //can be whatever you need, depends on test case
+        var sut = new MessageDialogs(messageBoxMock.Object);
 
-    //    bool result = sut.IsErrorDisplayed("");
-    //    if (!result)
-    //        result = sut.IsErrorDisplayed("error");
+        bool result = sut.IsErrorDisplayed("");
+        if (!result)
+            result = sut.IsErrorDisplayed("error");
 
-    //    Assert.IsTrue(result);
-    //}
+        Assert.IsTrue(result);
+    }
     //IMessageBox test
     //https://stackoverflow.com/questions/49146068/unit-tests-for-a-function-with-messagebox
 }
